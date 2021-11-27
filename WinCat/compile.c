577645377cbdfb4ba916404aef073aa223db2bc1
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-11-27 18:37:44 +0000

    Update compiler

--- WinCat/compile.c
+++ WinCat/compile.c
@@ -8,7 +8,13 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-	printf("Starting Compile!\n");
+	printf("Welcome to the WinCat compile script\n");
+	printf("Press enter to compile>");
+	scanf(">");
+	printf("\n");
 	system("cl main.cpp /oBuild/WinCat.exe");
+	printf("==================================================\n");
+	printf("Done! now check Wincat/Build for the built app \nIf it is not there check the message from the compiler for errors\n");
+	printf("==================================================\n");
 	return 0;
 }
